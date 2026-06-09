76527ddad4f08db4b5f7c218e202b0ac8dfc541c
Sudipta Samanta <sudiptasamanta917@gmail.com>
2026-06-09 15:14:49 +0530

    new

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({});
+export default defineConfig({
+  nitro: true,
+  tanstackStart: {
+    server: {
+      entry: "server",
+    },
+  },
+});
